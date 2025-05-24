3746ca2d7468a97c50691b27f7898c1ba32d932e
lew14748 <dimabdb@yandex.ru>
2025-05-24 14:25:49 +0700

    Implement Dictionary class and cover it with unit tests

--- src/main/java/logic/Dictionary.java
+++ src/main/java/logic/Dictionary.java
@@ -1,16 +1,39 @@
 package logic;
 
+import utils.IOAdapter;
+
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Dictionary {
-    ArrayList<String> Words = new ArrayList<>();
+    private ArrayList<String> Words = new ArrayList<>();
 
-    public Dictionary(String filename) throws FileNotFoundException {
+    public Dictionary(){};
 
+    public Dictionary(String filename) {
+        readWordsFromFile(filename);
     }
 
-    private void readWordsFromFile(String filename) {
+    public ArrayList<String> getWords() {
+        return Words;
+    }
 
+    boolean readWordsFromFile(String filename) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+
+            while (reader.ready()){
+                Words.add(reader.readLine());
+            }
+            return true;
+        } catch (FileNotFoundException e) {
+            IOAdapter.getInstance().write("FileNotFoundException: Dictionary not found");
+            return false;
+        } catch (IOException e) {
+            IOAdapter.getInstance().write("IOException: Exception during reading from dictionary");
+            return false;
+        }
     }
 }
---
+++ src/test/java/logic/DictionaryTest.java
@@ -0,0 +1,43 @@
+package logic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DictionaryTest {
+
+
+    private Dictionary dictionary;
+
+    @BeforeEach
+    void setUp(){
+        dictionary = new Dictionary();
+    }
+
+    @Test
+    @DisplayName("Correct words reading")
+    void readWordsFromFile_success(){
+        dictionary.readWordsFromFile("src/test/resources/TestDictionary.txt");
+        ArrayList<String> testDictionaryContent = new ArrayList<>(){
+            {
+                add("окно");
+                add("машина");
+                add("энтропия");
+            }
+        };
+        assertEquals(dictionary.getWords(), testDictionaryContent);
+    }
+
+    @Test
+    @DisplayName("Reading from non-existing file")
+    void readFromFile_fileNotFound(){
+        boolean readingStatus = dictionary.readWordsFromFile("src/test/resources/TestDictionary11.txt");
+        assertFalse(readingStatus);
+    }
+}
