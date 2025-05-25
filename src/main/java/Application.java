b4761a29c9f7b83c801b7724984a073cf2b200e7
lew14748 <dimabdb@yandex.ru>
2025-05-25 16:18:33 +0700

    Add reading test dictionary to Application class temporarily

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,6 +3,7 @@ import logic.Context;
 public class Application {
     public static void main(String[] argv) {
         Context context = new Context(new state.MainMenu());
+        context.setUp("src/test/resources/TestDictionary.txt");
         while (true)
             context.getCurrentAbstractState().run();
     }
