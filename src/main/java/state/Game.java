50eb3d1d494841d9efd9b3b49c88b3afe1a50233
lew14748 <dimabdb@yandex.ru>
2025-05-25 15:21:46 +0700

    Add state management in Game state run() method, add guessLetter() method

--- src/main/java/state/Game.java
+++ src/main/java/state/Game.java
@@ -3,20 +3,32 @@ package state;
 import utils.Menu;
 
 public class Game extends AbstractState {
-
+    @Override
     public void run() {
         render();
-        if(!getContext().getGame().getHiddenWord().guessLetter(String.valueOf(Menu.validateGameInput()))) {
-            getContext().getGame().decreaseLifeCount();
+        guessLetter();
+        if(getContext().getGame().checkLose()) {
+            goToAlternativeState();
+        } else {
+            if (getContext().getGame().checkWin()) {
+                goToNextState();
+            }
         }
-        //System.exit(0);
     }
 
+    @Override
     public void render() {
         adapter.clearConsole();
         getContext().getGame().getHiddenWord().printMaskedWord();
         adapter.write("Осталось жизней - " + getContext().getGame().getLifeCounter());
         //adapter.write("Game should be here");
+    }
+
+    private void guessLetter(){
+        String validatedGuessedLetter = String.valueOf(Menu.validateGameInput());
+        if(!validatedGuessedLetter.equals("WRONG INPUT")){
+            getContext().getGame().getHiddenWord().guessLetter(validatedGuessedLetter);
+        }
     }
 
     @Override
